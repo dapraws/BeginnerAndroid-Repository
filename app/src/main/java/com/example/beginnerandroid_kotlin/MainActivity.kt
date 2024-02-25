a4bce077a63a763d17f4f0e736c04aa330030216
dapraws <darrelprawira26@gmail.com>
2024-02-26 00:07:47 +0700

    update MainActivity.kt

--- app/src/main/java/com/example/beginnerandroid_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/beginnerandroid_kotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.beginnerandroid_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -18,6 +19,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
             }
         }
     }

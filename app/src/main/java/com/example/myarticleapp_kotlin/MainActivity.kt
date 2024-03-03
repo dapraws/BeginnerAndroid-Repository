f1339ed8d83afbf32a9638664fe4e165989dbbc3
dapraws <darrelprawira26@gmail.com>
2024-03-03 16:17:10 +0700

    update MainActivity.kt

--- app/src/main/java/com/example/myarticleapp_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/myarticleapp_kotlin/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.myarticleapp_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvArticle: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvArticle = findViewById(R.id.rv_article)
+        rvArticle.setHasFixedSize(true)
+
     }
 }

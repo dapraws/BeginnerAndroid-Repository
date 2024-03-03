b3e8d5ed49c8ea5c57ef2616fbe0e6703026bb2e
dapraws <darrelprawira26@gmail.com>
2024-03-03 15:07:15 +0700

    update data class

--- app/src/main/java/com/example/myarticleapp_kotlin/Article.kt
+++ app/src/main/java/com/example/myarticleapp_kotlin/Article.kt
@@ -1,7 +1,11 @@
 package com.example.myarticleapp_kotlin
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Article(
     val name: String,
     val description: String,
     val photo: Int
-)
+) : Parcelable

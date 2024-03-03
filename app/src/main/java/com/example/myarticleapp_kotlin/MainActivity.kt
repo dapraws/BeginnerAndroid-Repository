34e514bcb690b5be9c20a986aea3c973201e3a40
dapraws <darrelprawira26@gmail.com>
2024-03-03 16:18:58 +0700

    update MainActivity.kt

--- app/src/main/java/com/example/myarticleapp_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/myarticleapp_kotlin/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.myarticleapp_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var rvArticle: RecyclerView
+    private val list = ArrayList<Article>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +16,26 @@ class MainActivity : AppCompatActivity() {
         rvArticle = findViewById(R.id.rv_article)
         rvArticle.setHasFixedSize(true)
 
+        list.addAll(getListArticles())
+        showRecyclerList()
+
+    }
+
+    private fun getListArticles(): ArrayList<Article> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+        val listHero = ArrayList<Article>()
+        for (i in dataName.indices) {
+            val hero = Article(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+            listHero.add(hero)
+        }
+        return listHero
+    }
+
+    private fun showRecyclerList() {
+        rvArticle.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListArticleAdapter(list)
+        rvArticle.adapter = listHeroAdapter
     }
 }
